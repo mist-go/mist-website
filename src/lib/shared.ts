efbbdfece5f2a8d6d03fc36c09dd86d8f91fca80
Klesti Selimaj <klesti.selimay@icloud.com>
2026-05-11 00:35:05 +0200

    updated github repo

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -3,9 +3,8 @@ export const docsRoute = '/docs';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
-// fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
-  branch: 'main',
+  user: 'mist-go',
+  repo: 'mist',
+  branch: 'master',
 };
