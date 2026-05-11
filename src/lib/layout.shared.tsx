2b3da35889d6dda894620d226de22f7166908af6
Klesti Selimaj <klesti.selimay@icloud.com>
2026-05-11 03:31:31 +0200

    Logo

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,11 +1,16 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { appName, gitConfig } from "./shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
       // JSX supported
-      title: appName,
+      title: (
+        <div className="flex gap-2 items-center">
+          <img src="/logo.svg" alt="Logo" className="w-7 h-7" />
+          {appName}
+        </div>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
